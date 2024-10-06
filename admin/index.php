52e89e134b50fa95241acd20972750ac0665245c
OJT-jayrald <jayraldpelegrino26@gmail.com>
2024-10-06 13:46:23 +0800

    Putting Modal Bootstrap

--- admin/index.php
+++ admin/index.php
@@ -114,14 +114,67 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/4.0.1/css/fixedHeader.dataTables.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.3/css/responsive.dataTables.css">
+
+
 </head>
 
 <body>
-    <!-------LOGOUT--------->
-    WELCOME Admin: <?=$user['name'];?> | <a href="logout.php" onclick="return confirm('Are you sure you want to logout?')">Log Out</a>
-    <div id="divheader">
 
-        <form action="insert.php" method="post">
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="https://www.banbros.ph/assets/img/logowhite.png" style="width: 100px;" alt="logo"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#add" data-toggle="modal">Add New</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Welcome</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?=$user['name'];?> <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="logout.php" onclick="return confirm('Are you sure you want to logout?')">Log Out</a></li>
+            <li><a href="#">Profile</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Change Password</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+    <div id="divheader">
+        <!-- <form action="insert.php" method="post">
             <table width="100%" class="table border">
                 <tr>
                     <th align="left" style="text-align: center;">BANBROS PROPERTIES INVENTORY SYSTEM</th>
@@ -152,7 +205,7 @@
                     <td><button type="submit" id="submit" class="submit" name="submit"><span class="fa fa-save"></span> SUBMIT</button></td>
                 </tr>
             </table>
-        </form>
+        </form> -->
         <?php
             if(isset($_SESSION['success'])){
                 echo "<div style='background:green; color:#fff; padding:3px; border-radius:25px; font-size:20px; text-align:center;'>".$_SESSION['success']."</div>";
@@ -257,11 +310,16 @@
             <script src="https://cdn.datatables.net/fixedheader/4.0.1/js/fixedHeader.dataTables.js"></script>
             <script src="https://cdn.datatables.net/responsive/3.0.3/js/dataTables.responsive.js"></script>
             <script src="https://cdn.datatables.net/responsive/3.0.3/js/responsive.dataTables.js"></script>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
             <script>
                 new DataTable('#example', {
                     fixedHeader: true,
                     responsive: true
                 });
             </script>
+            <?php
+            include "modal_addnew.php";
+            ?>
     </body>
 </html>
---
+++ admin/modal_addnew.php
@@ -0,0 +1,47 @@
+<div class="modal fade" tabindex="-1" role="dialog" id="add">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">BANBROS PROPERTIES FORM</h4>
+      </div>
+      <div class="modal-body">
+        
+      <form action="insert.php" method="post">
+            <table width="100%" class="table border">
+                <tr>
+                    <td>Company Code<input type="text" name="company_code" placeholder="Company Code" required></td>
+                </tr>
+                <tr>
+                    <td>Assigned To<input type="text" name="assigned_to" placeholder="Name of the assignee" required></td>
+                </tr>
+                <tr>
+                    <td>
+                    <label for="location_n">Location</label>
+                    <select name="location_n">
+                    <option value="#">Select Department</option>
+                    <option value="accounting">Accounting</option>
+                    <option value="marketing">Marketing</option>
+                    </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Model Description<input type="text" name="model_description" placeholder="Model Description" required></td>
+                </tr>
+                <tr>
+                    <td>Serial Number<input type="text" name="serial_number" placeholder="NXE*********" required></td>
+                </tr>
+                <!-- <tr>
+                    <td><button type="submit" id="submit" class="submit" name="submit"><span class="fa fa-save"></span> SUBMIT</button></td>
+                </tr> -->
+            </table>
+
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="submit" name="submit" class="btn btn-primary">Add Items</button>
+        </form>
+      </di>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
