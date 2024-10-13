311f5e513fdf58d71e1914cecbba3aad764c5984
OJT-jayrald <jayraldpelegrino26@gmail.com>
2024-10-13 15:32:35 +0800

    Fixing

--- admin/index.php
+++ admin/index.php
@@ -18,7 +18,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory System</title>
-    <style>
+    
+<style>
     body{
         background-attachment: fixed;
         background-color: #eee;
